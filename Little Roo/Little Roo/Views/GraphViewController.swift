2e2cebf9aeb1878d52442f182a2cc7afc0afa709
Kate <kduncanwelke@gmail.com>
2020-12-02 17:12:20 -0600

    Date change capacity for graph

--- Little Roo/Little Roo/Views/GraphViewController.swift
+++ Little Roo/Little Roo/Views/GraphViewController.swift
@@ -14,16 +14,25 @@ class GraphViewController: UIViewController {
     
     @IBOutlet weak var graphView: LineChartView!
     @IBOutlet weak var type: UISegmentedControl!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
     @IBOutlet weak var bottomBannerAd: GADBannerView!
     
     private let kickViewModel = KickViewModel()
     private let graphViewModel = GraphViewModel()
+    
+    var date = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         updateChart()
+        
+        // start date is now, so you can't go back
+        forwardButton.isEnabled = false
+        formatDate()
         
         bottomBannerAd.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bottomBannerAd.rootViewController = self
@@ -57,7 +66,7 @@ class GraphViewController: UIViewController {
     }
     
     func updateChart() {
-        graphViewModel.getData(type: type.selectedSegmentIndex, date: Date())
+        graphViewModel.getData(type: type.selectedSegmentIndex, date: date)
         
         guard let dataSet = graphViewModel.getDataSet(), let data = graphViewModel.getLineChartData() else {
             return
@@ -102,10 +111,22 @@ class GraphViewController: UIViewController {
         let emptyVals = [Highlight]()
         graphView.highlightValues(emptyVals)
         
-        dataSet.colors = ChartColorTemplates.material()
+        //dataSet.colors = ChartColorTemplates.material()
         graphView.notifyDataSetChanged()
         
         graphView.animate(yAxisDuration: 0.5)
+    }
+    
+    func formatDate() {
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        var dateString = dateFormatter.string(from: date)
+        
+        if type.selectedSegmentIndex == 0 {
+            dayLabel.text = dateString
+        } else {
+            dayLabel.text = "Week of \(dateString)"
+        }
     }
 
     /*
@@ -117,10 +138,53 @@ class GraphViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: IBActions
+    
+    @IBAction func forwardPressed(_ sender: UIButton) {
+        // cancel if current date is today, you can't time travel
+        if Calendar.current.isDateInToday(date) {
+            return
+        }
+        
+        if type.selectedSegmentIndex == 0 {
+            // go back a day
+            date = Calendar.current.date(byAdding: .day, value: 1, to: date)!
+            updateChart()
+        } else {
+            // go back a week
+            date = Calendar.current.date(byAdding: .day, value: 7, to: date)!
+            updateChart()
+        }
+        
+        formatDate()
+    }
+    
+    @IBAction func backPressed(_ sender: UIButton) {
+        forwardButton.isEnabled = true
+        
+        if type.selectedSegmentIndex == 0 {
+            // go back a day
+            date = Calendar.current.date(byAdding: .day, value: -1, to: date)!
+            updateChart()
+        } else {
+            // go back a week
+            date = Calendar.current.date(byAdding: .day, value: -7, to: date)!
+            updateChart()
+        }
+        
+        if Calendar.current.isDateInToday(date) {
+            forwardButton.isEnabled = false
+        }
+        
+        formatDate()
+    }
     
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        // pop back to current date when switching day/week view
+        date = Date()
         updateChart()
+        formatDate()
     }
     
     @IBAction func back(_ sender: UIButton) {
